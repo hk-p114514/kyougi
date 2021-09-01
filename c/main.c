1ccd284e00b10ead559dee65feff45c80272b9af
hk-p114514 <hukada.gogotea@gmail.com>
2021-09-01 22:01:33 +0900

    問題の途中

--- c/main.c
+++ c/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+// https : // atcoder.jp/contests/abc215/tasks/abc215_b
 
 int main() {
+	int n;
+	scanf("%d", &n);
+
+	int k = 2, increase = 2;
+	while (increase <= n) {
+		// 2^k <= n の間繰り返す
+		increase *= k;
+	}
 
 	return (0);
 }
